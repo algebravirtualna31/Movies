4df9670d40d87b068fcb59ed51e9fe41c3051ba3
Kristijan Mihalinec <kristijan.mihalinec@king-ict.cloud>
2025-01-28 21:06:45 +0100

    Add project files.

---
+++ Movies.Data/Models/Movie.cs
@@ -0,0 +1,13 @@
+﻿namespace Movies.Data.Models
+{
+    public class Movie
+    {
+        public int Id { get; set; }
+
+        public string Title { get; set; }
+
+        public int Genre { get; set; }
+
+        public DateTime ReleaseYear { get; set; }
+    }
+}
---
+++ Movies.Data/Models/MoviesDbContext.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Movies.Data.Models
+{
+    public class MoviesDbContext : DbContext
+    {
+    }
+}
